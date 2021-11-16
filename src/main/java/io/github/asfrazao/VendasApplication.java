6b1c2d6c0cd0ab2ef94677178f92c8ec00f4a227
Alessandro Frazão <asfrazao@hotmail.com>
2021-11-16 07:10:54 -0300

    VendasJPA - Primeiro Comite Limpo

--- src/main/java/io/github/asfrazao/VendasApplication.java
+++ src/main/java/io/github/asfrazao/VendasApplication.java
@@ -1,17 +1,16 @@
 package io.github.asfrazao;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
-
-
 
     }
 }
